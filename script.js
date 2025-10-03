0eb54e8fe21d91938c2029a547e8e5f1cf82a1e6
ADITYApg-123 <aditya13pg@gmail.com>
2025-10-03 14:30:32 +0530

    Add first Javascript variables: name, age, and learning status

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ console.log("🚀 Welcome to JavaScript!");
 console.log("Ready to learn? Let's go!");
 
 // We'll add our code step by step below this line
+
+// Variables - containers for storing data
+let myName = "Aditya";
+let myAge = 25;
+let isLearning = true;
+
+console.log("My name is", myName);
+console.log("My age is:", myAge);
+console.log("Am I learning JavaScript?", isLearning);

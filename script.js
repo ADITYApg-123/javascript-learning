87372991d03409e0490d5abb38ee71f2bf29056c
ADITYApg-123 <aditya13pg@gmail.com>
2025-10-03 14:21:42 +0530

    Initial setup: Add HTML and JS files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Day 1: JavaScript Basics
+// We'll start our JavaScript journey here!
+
+console.log("🚀 Welcome to JavaScript!");
+console.log("Ready to learn? Let's go!");
+
+// We'll add our code step by step below this line

2998dd3a67f0990e014ce4f94f9c3d8c9043da52
ADITYApg-123 <aditya13pg@gmail.com>
2025-10-03 14:49:24 +0530

    Add daatypes demo: strings, numbers, booleans, array, objects

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ let isLearning = true;
 console.log("My name is", myName);
 console.log("My age is:", myAge);
 console.log("Am I learning JavaScript?", isLearning);
+
+// Different Data Types
+let studentName = "Sarah";  //string
+let score = 95.5;   // Numbers(can be decimal)
+let isGraduated = false; // Boolean
+let favouriteColors = ["blue", "green", "red"]; //Array
+let student = {name: "Alex", grade: "A"};  //Object
+
+console.log("=== Data Types Demo ===");
+console.log("Name:", studentName, "Type:", typeof studentName);
+console.log("Score:", score, "Type:", typeof score);
+console.log("Graduated:", isGraduated, "Type:", typeof isGraduated);
+console.log("Favourite Colors:", favouriteColors, "Type:", typeof favouriteColors);
+console.log("Student Object:", student, "Type:", typeof student);
